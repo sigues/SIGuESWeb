c3c4831883ec47ee11574624b89b7a62e49b3ed7
salvador <salvador@ubuntu>
2012-10-26 00:09:23 -0500

    Pruebas de nusoap

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -2,7 +2,41 @@
 
 class Welcome extends CI_Controller {
 
-	public function index()
+    function __construct() {
+        parent::_construct();
+        $this->load->library("nuSoap_lib");
+    }
+
+       function index() {
+//echo "hola";die();
+           
+           $this->load->library("prueba");
+           echo $this->prueba->prueba();
+           $this->load->library("NuSOAP/lib/nusoap");
+           echo "-".$this->nusoap->getGlobalDebugLevel();
+           /*$this->nusoap_client = new nusoap_client("http://localhost/SIGuESCore/app_dev.php/soap");
+
+           if($this->nusoap_client->fault)
+           {
+                $text = 'Error: '.$this->nusoap_client->fault;
+           }
+           else
+           {
+               if ($this->nusoap_client->getError())
+               {
+                    $text = 'Error: '.$this->nusoap_client->getError();
+               }
+               else
+               {
+                    $row = $this->nusoap_client->call(
+                             'response',
+                              array("parametro"=>true)
+                           );
+               }
+           }*/
+       }
+        
+	public function index_xml()
 	{
                 $this->load->helper('url');
 		$server_url = 'http://localhost/SIGuESCore/index.php/siguescore/';//
---
+++ application/libraries/prueba.php
@@ -0,0 +1,9 @@
+<?php
+
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+class prueba{
+    function prueba(){
+        return "prueba";
+        //Por si estamos ejecutando este script en un servidor Windows
+    }
+}
