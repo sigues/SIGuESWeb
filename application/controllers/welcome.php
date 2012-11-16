97da84aaac16fac21b74cd0ea70265ae6d28656e
Salvador Villegas Mancilla <salvador@sigues.org>
2012-11-16 00:44:33 -0600

    Validación de app y conexion a múltiples bases
    Validación de app y conexion a múltiples bases

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -2,38 +2,48 @@
 
 class Welcome extends CI_Controller {
 
-    function __construct() {
-        parent::_construct();
-        $this->load->library("nuSoap_lib");
-    }
+        function index() {
+            $this->load->helper('url');
+            $server_url = 'http://localhost/SIGuESCore/index.php/siguescore/';//
 
-       function index() {
-//echo "hola";die();
-           
-           $this->load->library("prueba");
-           echo $this->prueba->prueba();
-           $this->load->library("NuSOAP/lib/nusoap");
-           echo "-".$this->nusoap->getGlobalDebugLevel();
-           /*$this->nusoap_client = new nusoap_client("http://localhost/SIGuESCore/app_dev.php/soap");
+            $this->load->library('xmlrpc');
 
-           if($this->nusoap_client->fault)
-           {
-                $text = 'Error: '.$this->nusoap_client->fault;
-           }
-           else
-           {
-               if ($this->nusoap_client->getError())
-               {
-                    $text = 'Error: '.$this->nusoap_client->getError();
-               }
-               else
-               {
-                    $row = $this->nusoap_client->call(
-                             'response',
-                              array("parametro"=>true)
-                           );
-               }
-           }*/
+            $this->xmlrpc->server($server_url, 80);
+            $this->xmlrpc->method('Greetings');
+
+            $request = array('bolas','peludas','cagontoo');
+            $this->xmlrpc->request($request);
+
+            if ( ! $this->xmlrpc->send_request())
+            {
+                echo $this->xmlrpc->display_error();
+            }
+            else
+            {
+                echo '<pre>';
+                print_r($this->xmlrpc->display_response());
+                echo '</pre>';
+            }
+
+
+            $this->xmlrpc->method('getUsuarioByUserPass');
+
+            $request = array('test_admin',md5('test_pass'));
+            $this->xmlrpc->request($request);
+
+            if ( ! $this->xmlrpc->send_request())
+            {
+                echo $this->xmlrpc->display_error();
+            }
+            else
+            {
+                echo '<pre>';
+                print_r($this->xmlrpc->display_response());
+                echo '</pre>';
+            }
+
+
+
        }
         
 	public function index_xml()
